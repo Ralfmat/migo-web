7010789c6d85f90f5fd49b9156f0288c44e61e63
koload <koload20@gmail.com>
2024-12-11 18:54:49 +0100

    header

--- wizytowka/src/components/Header.js
+++ wizytowka/src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Welcome to Our Project</h1>
+    <header className="header">
+      <div className="logo">A migo</div>
+      <nav className="navigation">
+        <ul>
+          <li><a href="#business-plan">Business Plan</a></li>
+          <li><a href="#description">Description</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="#footer">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
